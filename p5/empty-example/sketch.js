eab594e2fae754243dd297c875732c34900cfa2f
ilei2 <ilei2@illinois.edu>
2017-05-17 18:36:20 -0500

    bird.js and pipes.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,13 +1,37 @@
+var bird;
+var pipes = [];
+
 function setup(){
-  createCanvas(640, 480);
+  createCanvas(400,600);
+  bird = new Bird;
+  pipes.push(new Pipe());
 }
+
 function draw(){
-  if (mouseIsPressed){
-    fill(0);
+  background(0);
+  bird.update();
+  bird.show();
+
+  if (frameCount % 150 == 0){
+    pipes.push(new Pipe());
   }
-  else{
-    fill(255);
+
+  for (var i = 0; i < pipes.length; i ++){
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)){
+      console.log("HIT");
+    }
+
+    if (pipes[i].x < -width){  //goes off screen
+      pipes.splice(i,1); //deletes element from array
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80)
-  //ellipse(50,50,80,80);
+}
+
+function keyPressed(){
+  if (key == ' '){
+    bird.up();
+  }
 }
